eeb0d43c47a64530be355883c15e57a20238fa78
z.taibi <z.taibi@egioteam.com>
2023-06-15 12:19:23 +0100

    bouton Ajouter

--- src/app/visualisation/visualisation.component.ts
+++ src/app/visualisation/visualisation.component.ts
@@ -1,31 +1,72 @@
-import { Component,OnInit } from '@angular/core';
-import { FormBuilder, FormGroup} from '@angular/forms';
+import { Component,OnInit,AfterViewInit } from '@angular/core';
+import {  FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-visualisation',
   templateUrl: './visualisation.component.html',
   styleUrls: ['./visualisation.component.css']
 })
-export class VisualisationComponent implements OnInit{
+export class VisualisationComponent implements OnInit,AfterViewInit{
   visualisationForm !: FormGroup;
-  isDescriptionVisible = false;
-  isActive = false;
+ 
+
 
 
   constructor(private formBuilder: FormBuilder){
 
   }
   ngOnInit(): void {
-
+    
   }
-  toggleDescription(): void {
-    this.isDescriptionVisible = !this.isDescriptionVisible;
+  ngAfterViewInit(): void {
+    const addButton = document.getElementById("addButton");
+    if (addButton) {
+      addButton.addEventListener("click", this.ajouterBloc.bind(this));
+    }
   }
 
-  toggleActiveState(): void {
-    this.isActive = !this.isActive;
+  ajouterBloc() {
+    const blocExistant = document.querySelector(".blocks") as HTMLElement;
+    if (blocExistant) {
+      const nouveauBloc = blocExistant.cloneNode(true) as HTMLElement;
+      const container = document.getElementById("container");
+      if (container) {
+        container.appendChild(nouveauBloc);
+      }
+    }
   }
-
- 
- 
+  ajouterBlocEdu() {
+    const blocExistant = document.querySelector(".blocksEdu") as HTMLElement;
+    if (blocExistant) {
+      const nouveauBloc = blocExistant.cloneNode(true) as HTMLElement;
+      const containerEdu = document.getElementById("containerEdu");
+      if (containerEdu) {
+        containerEdu.appendChild(nouveauBloc);
+      }
+    }
+  }
+  ajouterBlocLang() {
+    const blocExistant = document.querySelector(".blocksLang") as HTMLElement;
+    if (blocExistant) {
+      const nouveauBloc = blocExistant.cloneNode(true) as HTMLElement;
+      const containerLang = document.getElementById("containerLang");
+      if (containerLang) {
+        containerLang.appendChild(nouveauBloc);
+      }
+    }
+  }
+  ajouterBlocCertif() {
+    const blocExistant = document.querySelector(".blocksCertif") as HTMLElement;
+    if (blocExistant) {
+      const nouveauBloc = blocExistant.cloneNode(true) as HTMLElement;
+      const containerCertif = document.getElementById("containerCertif");
+      if (containerCertif) {
+        containerCertif.appendChild(nouveauBloc);
+      }
+    }
+  }
 }
+  
+
+
+
