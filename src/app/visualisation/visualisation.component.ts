8ffeb9eb64cb951a7ed6b23bd3f9a077d20e73c3
z.taibi <z.taibi@egioteam.com>
2023-06-21 12:17:32 +0100

    modif visual

--- src/app/visualisation/visualisation.component.ts
+++ src/app/visualisation/visualisation.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit,AfterViewInit } from '@angular/core';
-import {  FormBuilder, FormGroup} from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { createClient} from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-visualisation',
@@ -8,15 +9,27 @@ import {  FormBuilder, FormGroup} from '@angular/forms';
 })
 export class VisualisationComponent implements OnInit,AfterViewInit{
   visualisationForm !: FormGroup;
- 
+  supabaseUrl!: string;
+  supabaseKey!: string;
+  supabase: any;
 
 
 
   constructor(private formBuilder: FormBuilder){
-
+    this.supabaseUrl = 'https://mljtanxsvdnervhrjnbs.supabase.co';
+    this.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sanRhbnhzdmRuZXJ2aHJqbmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4NDczMDQsImV4cCI6MjAwMDQyMzMwNH0.lrhe---iFdN9RSFGgF5cYwN9S_aWpxYGur1TAvrD-ZY';
+    this.supabase = createClient(this.supabaseUrl, this.supabaseKey);
   }
   ngOnInit(): void {
-    
+    const telephonePattern = /^\d{4}\.\d{3}\.\d{3}$/;
+    this.visualisationForm = this.formBuilder.group({
+      nom: ['',Validators.required],
+      prenom: ['',Validators.required],
+      email: ['',Validators.email],
+      telephone: ['',Validators.pattern(telephonePattern)],
+      role: [''],
+      Annéesexpérience: ['']
+    });
   }
   ngAfterViewInit(): void {
     const addButton = document.getElementById("addButton");
@@ -24,7 +37,6 @@ export class VisualisationComponent implements OnInit,AfterViewInit{
       addButton.addEventListener("click", this.ajouterBloc.bind(this));
     }
   }
-
   ajouterBloc() {
     const blocExistant = document.querySelector(".blocks") as HTMLElement;
     if (blocExistant) {
@@ -75,7 +87,25 @@ export class VisualisationComponent implements OnInit,AfterViewInit{
       }
     }
   }
+  onSubmit(): void {
+    if (this.visualisationForm.valid) {
+      const formData = this.visualisationForm.value;
+        this.supabase
+        .from('candidat')
+        .insert([formData])
+        .then((response: any) => {
+          console.log('Enregistrement réussi :', response);
+          alert('Enregistrement réussi.');
+          this.visualisationForm.reset();
+        })
+        .catch((error: any) => {
+          alert('Erreur lors de l\'enregistrement.');
+          console.error("Erreur lors de l'enregistrement :", error);
+        });
+    }
+  }
 }
+
   
 
 
